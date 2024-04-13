bd80c68487cee9cc4f8e3da54218f84a3296cba5
saadli wissem <saadliwissem88@gmail.com>
2024-04-13 16:31:30 +0100

    removed log

--- middleWares/jerifyToken.js
+++ middleWares/jerifyToken.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const token = req.header("Authorization");
-  console.log(token)
   if (!token) {
     return res.status(401).json({ error: "Unauthorized - No token provided" });
   }
