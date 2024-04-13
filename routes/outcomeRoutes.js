425632e85b1b04278ebf647028b0cf25895fae89
saadli wissem <saadliwissem88@gmail.com>
2024-04-13 16:29:16 +0100

    verification works

--- routes/outcomeRoutes.js
+++ routes/outcomeRoutes.js
@@ -15,13 +15,13 @@ const router = express.Router();
 
 // Define routes for Outcome model
 router.post("/:userId", verifyToken, createOutcome); // Create a new outcome
-router.get("/:userId", getAllOutcomes); // Get all outcomes
-router.get("/outcome/:id", getOutcome); // Get a specific outcome by ID
-router.put("/:id/increase", increaseOutcome); // Increase the value of an outcome by ID
-router.delete("/:id", deleteOutcome); // Delete an outcome by ID
-router.post("/delete/:id", deleteOutcomes); // Delete multiple outcomes by IDs
+router.get("/:userId", verifyToken, getAllOutcomes); // Get all outcomes
+router.get("/outcome/:id", verifyToken, getOutcome); // Get a specific outcome by ID
+router.put("/:id/increase", verifyToken, increaseOutcome); // Increase the value of an outcome by ID
+router.delete("/:id", verifyToken, deleteOutcome); // Delete an outcome by ID
+router.post("/delete/:id", verifyToken, deleteOutcomes); // Delete multiple outcomes by IDs
 
-router.delete("/:id/suggestions/:index", deleteSuggestion); // Delete a suggestion from an outcome by ID and suggestion index
-router.post("/:id/suggestions", addSuggestion); // Add a suggestion to an outcome by ID
+router.delete("/:id/suggestions/:index", verifyToken, deleteSuggestion); // Delete a suggestion from an outcome by ID and suggestion index
+router.post("/:id/suggestions", verifyToken, addSuggestion); // Add a suggestion to an outcome by ID
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,14 +8,15 @@ const {
   resendCode,
   getUserById,
 } = require("../controllers/UserController");
+const verifyToken = require("../middleWares/jerifyToken");
 const router = express.Router();
 
 // Define routes for User model
 router.post("/register", register); // Create a new user
 router.post("/login", login); // login user
 router.get("/get-user", getUserById);
-router.post("/uploadProfileImage", uploadProfileImage);
-router.put("/changepwd", changePassword);
+router.post("/uploadProfileImage", verifyToken, uploadProfileImage);
+router.put("/changepwd", verifyToken, changePassword);
 router.post("/verifyUser", CodeVerification);
 router.post("/resendcode", resendCode);
 
